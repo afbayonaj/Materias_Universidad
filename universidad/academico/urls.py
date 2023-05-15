670102718351060c7ed934751f1e07c08fb707a8
Andres Bayona <andres.bayona@outlook.com>
2023-05-15 09:24:23 -0500

    nuevo cambio en el path para url quede index

--- universidad/academico/urls.py
+++ universidad/academico/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('index', views.home),
     path('registrar_curso/', views.registrar_curso),
     path('edicion_cursos/<codigo>', views.edicion_curso),
     path('editar_curso/', views.editar_curso),
